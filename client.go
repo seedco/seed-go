3028c207251be89f94db7455c31c9fbc69c95c75
John Cai <johncai@seed.co>
2017-06-10 16:06:21 -0700

    handle errors (#3)

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package seed
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -16,6 +17,7 @@ type Client struct {
 	httpClient    *http.Client
 }
 
+// New creates a new Seed client given an access token
 func New(accessToken string) *Client {
 	return &Client{
 		httpClient:  &http.Client{},
@@ -23,6 +25,7 @@ func New(accessToken string) *Client {
 	}
 }
 
+// SetClientVersion sets the client version that each request will be made with
 func (c *Client) SetClientVersion(clientVersion string) {
 	c.clientVersion = clientVersion
 }
@@ -38,4 +41,16 @@ func (c *Client) do(req *http.Request) (*http.Response, error) {
 type Pages struct {
 	Next     *url.Values `json:"next"`
 	Previous *url.Values `json:"previous"`
+}
+
+type ErrorList []map[string]string
+
+func (e ErrorList) Error() string {
+	buf := bytes.NewBufferString("")
+	for _, e := range e {
+		errorString := fmt.Sprintf("field: %s, message: %s", e["field"], e["message"])
+		buf.WriteString(errorString)
+		buf.WriteString("\n")
+	}
+	return buf.String()
 }
